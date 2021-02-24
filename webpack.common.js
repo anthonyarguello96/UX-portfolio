c08b48dbd52e77ccd7d96ddb3a2cff7b70b6099c
anthonyarguello96 <anthonyarguello96@gmail.com>
2021-02-24 15:19:43 -0500

    Add Image minimizer

--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+
 
 
 module.exports = {
@@ -29,4 +31,43 @@ module.exports = {
   }
     ]
   }
+};
+
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader', // Or `url-loader` or your other loader
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new ImageMinimizerPlugin({
+      minimizerOptions: {
+        // Lossless optimization with custom option
+        // Feel free to experiment with options for better result for you
+        plugins: [
+          ['gifsicle', { interlaced: true }],
+          ['jpegtran', { progressive: true }],
+          ['optipng', { optimizationLevel: 5 }],
+          [
+            'svgo',
+            {
+              plugins: [
+                {
+                  removeViewBox: false,
+                },
+              ],
+            },
+          ],
+        ],
+      },
+    }),
+  ],
 };

00913aca23957528abfbc0a5f5c99c266ef559fc
anthonyarguello96 <anthonyarguello96@gmail.com>
2021-02-26 13:26:42 -0500

    Style home page

--- src/index.js
+++ src/index.js
@@ -9,3 +9,30 @@ lib.displayMenu('nav-toggle', 'nav-menu');
 //Hide navigration menu after click
 const navLink = document.querySelectorAll('.nav__link');
 navLink.forEach(x => x.addEventListener('click', lib.linkAction))
+
+
+//scroll sections active link
+const sections = document.querySelectorAll('section[id]');
+
+
+const scrollActive = () => {
+  const scrollY= window.pageYOffset;
+
+  sections.forEach(current =>{
+    const sectionHeight = current.OffsetHeight;
+    const sectionTop = current.OffsetTop - 50;
+    console.log(sectionHeight);
+    console.log(sectionTop);
+    sectionId = current.getAttriibute('id');
+
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+      document.querySelector('nav__menu a[href*='+ sectionId+']').classList.add('active')
+    }else{
+      document.querySelector('.nav__menu a[href*=]'+ sectionId+']').classList.add('active')
+    }
+  })
+}
+
+
+
+window.addEventListener('scroll', scrollActive);

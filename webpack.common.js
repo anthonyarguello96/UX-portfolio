85486ae8d17502d52fdc4006135120a12c88c4b3
anthonyarguello96 <anthonyarguello96@gmail.com>
2021-03-26 16:55:38 -0400

    Comment out html-loader
    There is a problem between html-loader and file-loader
    so I deactivate html-loader to try to work the problem out.

--- webpack.common.js
+++ webpack.common.js
@@ -1,73 +1,93 @@
 const path = require("path");
 const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
+
+//
+// module.exports = {
+//     devtool: false,
+//   mode: 'development',
+//   entry: "./src/index.js",
+//   output:{
+//     filename: "main.[contenthash].js",
+//     path: path.resolve(__dirname, "dist")
+//   },
+//   module:{
+//     rules: [
+//       {
+//        test: /\.html$/i,
+//        loader: 'html-loader',
+//      },
+//      {
+//     test: /\.(svg|png|jpe?g|gif)$/i,
+//     use: [
+//       {
+//         loader: 'file-loader',
+//         options:{
+//           name:"[name].[hash].[ext]",
+//           outputPath: "imgs"
+//         }
+//       },
+//     ],
+//   }
+//     ]
+//   }
+// };
+//
 
 module.exports = {
-    devtool: false,
-  mode: 'development',
+  devtool: false,
   entry: "./src/index.js",
-  output:{
-    filename: "main.[contenthash].js",
-    path: path.resolve(__dirname, "dist")
-  },
-  module:{
-    rules: [
-      {
-       test: /\.html$/i,
-       loader: 'html-loader',
-     },
-     {
-    test: /\.(svg|png|jpe?g|gif)$/i,
-    use: [
-      {
-        loader: 'file-loader',
-        options:{
-          name:"[name].[hash].[ext]",
-          outputPath: "imgs"
-        }
-      },
-    ],
-  }
-    ]
-  }
-};
-
-
-module.exports = {
   module: {
     rules: [
+      // {
+      //   test: /\.html$/,
+      //   type: 'asset/resource',
+      //   generator: {
+      //     filename: '[name][ext]',
+      //   },
+      // },
+      // {
+      //   test: /\.html$/i,
+      //   use: ['extract-loader', 'html-loader'],
+      // },
       {
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        use: [
-          {
-            loader: 'file-loader', // Or `url-loader` or your other loader
-          },
-        ],
-      },
+     test: /\.(svg|png|jpe?g|gif)$/i,
+     use: [
+       {
+         loader: 'file-loader',
+         options:{
+           name:"[name].[hash].[ext]",
+           outputPath: "imgs",
+           esModule: false
+         }
+       },
+     ],
+   },
     ],
   },
-  plugins: [
-    new ImageMinimizerPlugin({
-      minimizerOptions: {
-        // Lossless optimization with custom option
-        // Feel free to experiment with options for better result for you
-        plugins: [
-          ['gifsicle', { interlaced: true }],
-          ['jpegtran', { progressive: true }],
-          ['optipng', { optimizationLevel: 5 }],
-          [
-            'svgo',
-            {
-              plugins: [
-                {
-                  removeViewBox: false,
-                },
-              ],
-            },
-          ],
-        ],
-      },
-    }),
-  ],
+//   plugins: [
+//     new ImageMinimizerPlugin({
+//       minimizerOptions: {
+//         // Lossless optimization with custom option
+//         // Feel free to experiment with options for better result for you
+//         plugins: [
+//           ['gifsicle', { interlaced: true }],
+//           ['jpegtran', { progressive: true }],
+//           ['optipng', { optimizationLevel: 5 }],
+//           [
+//             'svgo',
+//             {
+//               plugins: [
+//                 {
+//                   removeViewBox: false,
+//                 },
+//               ],
+//             },
+//           ],
+//         ],
+//       },
+//     }),
+//   ],
 };
